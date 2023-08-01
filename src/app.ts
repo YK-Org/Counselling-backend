50691ede27f48af47d15fe1c97177010f4a9fce1
Matekuor <matekuor94@gmail.com>
2023-08-01 07:28:48 +0000

    middleware to check auth

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,13 @@ import express from "express";
 import cors from "cors";
 import routes from "./routes";
 import dotenv from "dotenv";
+import MiddlewareService from "./middleware/index";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-
+app.use(MiddlewareService.checkAuthentication);
 app.use("/api/v1", routes);
 
 export default app;
--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -5,23 +5,27 @@ import { validate } from "class-validator";
 
 class MiddlewareService {
   checkAuthentication = (req: any, res: Response, next: any) => {
-    const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1];
+    if (req.path.includes("login")) {
+      next();
+    } else {
+      const authHeader = req.headers["authorization"];
+      const token = authHeader && authHeader.split(" ")[1];
 
-    if (token == null) return res.sendStatus(401);
-    jwt.verify(
-      token,
-      process.env.TOKEN_SECRET as string,
-      (err: any, user: any) => {
-        console.log(err);
+      if (token == null) return res.sendStatus(401);
+      jwt.verify(
+        token,
+        process.env.TOKEN_SECRET as string,
+        (err: any, user: any) => {
+          console.log(err);
 
-        if (err) return res.sendStatus(403);
+          if (err) return res.sendStatus(403);
 
-        req.user = user;
+          req.user = user;
 
-        next();
-      }
-    );
+          next();
+        }
+      );
+    }
   };
 
   requestValidation = (validationClass: any) => {
