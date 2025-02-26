66f8a22ad597fc0fcbc859c675f6262865f38c8f
Matekuor <matekuor94@gmail.com>
2025-02-26 07:56:04 +0000

    test

--- src/helpers/generatePassword.ts
+++ src/helpers/generatePassword.ts
@@ -7,5 +7,6 @@ export function generatePassword() {
     const randomNumber = Math.floor(Math.random() * chars.length);
     password += chars.substring(randomNumber, randomNumber + 1);
   }
+
   return password;
 }
